144427aa44231c17ab9e073d5e478123e06e711d
Harry Bradshaw <harry.bradshaw@softwire.com>
2024-09-13 11:14:14 +0100

    Initial commit

---
+++ src/components/Calendar/Calendar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export interface CalendarProps {
+
+}
+
+const Calendar = (props: CalendarProps) => {
+    return (
+        <div>
+            This is a calendar
+        </div>
+    )
+}
+
+export default Calendar;
